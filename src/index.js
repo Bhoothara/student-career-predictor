60d6981fc02b9be4a7748f81f568146331b554b4
Bhoothara <bhootharajume08@gmail.com>
2025-06-26 09:16:46 +0530

    Add PWA support

--- src/index.js
+++ src/index.js
@@ -9,3 +9,14 @@ root.render(
     <App />
   </BrowserRouter>
 );
+
+// ✅ Register service worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').then((registration) => {
+      console.log('SW registered: ', registration);
+    }).catch((registrationError) => {
+      console.log('SW registration failed: ', registrationError);
+    });
+  });
+}
